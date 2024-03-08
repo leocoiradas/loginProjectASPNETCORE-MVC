b19afacd81927e90cb07736b1577f8f720188894
leocoiradas <coiradasalan@gmail.com>
2024-03-08 17:49:29 -0300

    Agregar archivos de proyecto.

---
+++ loginProyectASPNETCORE-MVC/Models/DbpruebaContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace loginProyectASPNETCORE_MVC.Models;
+
+public partial class DbpruebaContext : DbContext
+{
+    public DbpruebaContext()
+    {
+    }
+
+    public DbpruebaContext(DbContextOptions<DbpruebaContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("PK__USUARIO__1788CCACD0A5D2F7");
+
+            entity.ToTable("USUARIO");
+
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+            entity.Property(e => e.Email)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Password)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Username)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ loginProyectASPNETCORE-MVC/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace loginProyectASPNETCORE_MVC.Models;
+
+public partial class Usuario
+{
+    public int UserId { get; set; }
+
+    public string? Username { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Password { get; set; }
+}
