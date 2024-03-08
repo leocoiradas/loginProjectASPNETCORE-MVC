b790e1a4159cb87c34be9737d70fb63cd4f15952
leocoiradas <coiradasalan@gmail.com>
2024-03-08 18:19:12 -0300

    Creado contexto para la conexión a la base de datos SQL server

--- loginProyectASPNETCORE-MVC/Program.cs
+++ loginProyectASPNETCORE-MVC/Program.cs
@@ -12,6 +12,15 @@ namespace loginProyectASPNETCORE_MVC
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            //Configuración del contexto de mi base de datos para tener acceso a ella desde la aplicación
+            //Nota: DbpruebaContext se refiere a la clase usada para conectar a nuestra base de datos de SQL server
+            //Con este contexto, generamos la conexión a la db mediante la obtención de la cadena de conexión que se esttableció en
+            //appsettings.json
+            builder.Services.AddDbContext<DbpruebaContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("SQLString"));
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
