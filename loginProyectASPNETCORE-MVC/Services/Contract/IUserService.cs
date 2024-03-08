00d8baebd3921fcdd35fd50a23b4b2642a41094c
leocoiradas <coiradasalan@gmail.com>
2024-03-08 20:18:29 -0300

    Creada interface para gestionar la obtención y guardado de usuario

---
+++ loginProyectASPNETCORE-MVC/Services/Contract/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using loginProyectASPNETCORE_MVC.Models;
+
+namespace loginProyectASPNETCORE_MVC.Services.Contract
+{
+    public interface IUserService
+    {
+        Task<Usuario> GetUser(string email, string password);
+        Task<Usuario> SaveUser(Usuario model);
+    }
+}
