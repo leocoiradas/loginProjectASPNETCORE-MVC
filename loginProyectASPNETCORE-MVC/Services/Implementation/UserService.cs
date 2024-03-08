381730cb96d098f1eff4bccd1b5635954a9b9267
leocoiradas <coiradasalan@gmail.com>
2024-03-08 20:20:24 -0300

    Implementación de IUserService + lógica de los métodos
    Se implementó la lógica de los métodos declarados en el interface de IUserService y se implementó la lógica de los mismos en esta clase. Cada método obtiene un usuario y añade un usuario a la bd respectivamente.

---
+++ loginProyectASPNETCORE-MVC/Services/Implementation/UserService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using loginProyectASPNETCORE_MVC.Models;
+using loginProyectASPNETCORE_MVC.Services.Contract;
+
+namespace loginProyectASPNETCORE_MVC.Services.Implementation
+{
+    public class UserService : IUserService
+    {
+        private readonly DbpruebaContext _dbpruebaContext;
+
+        public UserService(DbpruebaContext dbContext)
+        {
+            _dbpruebaContext = dbContext;
+        }
+        public async Task<Usuario> GetUser(string email, string password)
+        {
+            Usuario user_found = await _dbpruebaContext.Usuarios.Where(u => u.Email == email && u.Password == password).FirstOrDefaultAsync();
+            return user_found;
+        }
+
+        public async Task<Usuario> SaveUser(Usuario model)
+        {
+            _dbpruebaContext.Usuarios.Add(model);
+            _dbpruebaContext.SaveChangesAsync();
+            return model;
+        }
+    }
+}
