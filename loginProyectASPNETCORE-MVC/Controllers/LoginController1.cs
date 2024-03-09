198bbcc4e0dc7327513a86931401658149cf5739
leocoiradas <coiradasalan@gmail.com>
2024-03-08 21:58:20 -0300

    Creado controlador de registro
    Se creó el controlador encargado de manejar el registro e inicio de sesión del usuario. De momento se implementó el método encargado de manejar el registro del usuario, encriptando la contraseña, guardando el usuario en la db, y redirigiendo al usuario al home si la operación se realiza con éxito. De lo contrario devuelve un mensaje de error.
    
    Nota: Se utiliza la inyección de dependencias para usar los métodos declarados en IUserServices e implementar su lógica aquí.

---
+++ loginProyectASPNETCORE-MVC/Controllers/LoginController1.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using loginProyectASPNETCORE_MVC.Models;
+using loginProyectASPNETCORE_MVC.Resources;
+using loginProyectASPNETCORE_MVC.Services.Contract;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+
+namespace loginProyectASPNETCORE_MVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IUserService _userService;
+        public LoginController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task <IActionResult> SignUp(Usuario user)
+        {
+            user.Password = Utilities.EncryptPassword(user.Password);
+            Usuario user_created = await _userService.SaveUser(user);
+            if (user_created.UserId > 0)
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            ViewData["Message"] = "User could not be created";
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string user, string password)
+        {
+            return View();
+        }
+    }
+}
