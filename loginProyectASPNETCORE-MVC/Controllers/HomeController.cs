efc9af166739b5cdd5857a4bd0a83c825626502b
leocoiradas <coiradasalan@gmail.com>
2024-03-12 16:28:26 -0300

    HomeController: Implementado método Logout y acceso sólo para usuarios autorizados
    Nota: Con el prefijo [authorize] de arriba establecemos que sólo los usuarios autorizados tengan acceso a los métodos del controlador. Si un usuario intenta ir a una ruta o ejecutar un controlador o método que requiere autorización o autenticación será redirigido a la ruta por defecto (la ruta del formulario de inicio de sesión establecida en la clase Program).

--- loginProyectASPNETCORE-MVC/Controllers/HomeController.cs
+++ loginProyectASPNETCORE-MVC/Controllers/HomeController.cs
@@ -1,10 +1,14 @@
 using loginProyectASPNETCORE_MVC.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.Security.Claims;
 
 namespace loginProyectASPNETCORE_MVC.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -37,6 +41,13 @@ namespace loginProyectASPNETCORE_MVC.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task <IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return RedirectToAction("Login", "Login");
         }
     }
 }
